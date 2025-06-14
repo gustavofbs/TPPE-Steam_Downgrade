273686bcbbf466672fcb88baf8a420d8078d6d53
gustavofbs <guguboasorte@gmail.com>
2025-06-14 09:41:34 -0300

    initial vBeta

--- steam_downgrade_python/hello_world/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.hello_world, name='hello_world'),
-    path('api/hello/', views.HelloWorldAPIView.as_view(), name='hello_api'),
-]
